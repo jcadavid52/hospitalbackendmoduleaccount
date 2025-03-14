a3e5ff26fa305d454dd1deb740590c19b6aa2400
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-03-13 19:22:26 -0500

    agrego capa de api

---
+++ HospitalModuleAccount.Api/ApiHandlers/AccountApi/AccountUserRegisterCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HospitalModuleAccount.Application.Account.CommandHandler.Command;
+
+namespace HospitalModuleAccount.Api.ApiHandlers.AccountApi
+{
+    public class AccountUserRegisterCommandValidator: AbstractValidator<AccountRegisterCommand>
+    {
+        public AccountUserRegisterCommandValidator()
+        {
+            RuleFor(command => command.fullName)
+         .NotEmpty()
+         .WithMessage("El campo usuario no puede estar vacío");
+
+        }
+    }
+}
